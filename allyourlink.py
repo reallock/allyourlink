58a1c7b3f3ccc0aaba06bd1f0547f7d7a9b0222e
reallock <42710202+reallock@users.noreply.github.com>
2023-05-08 06:12:03 -0400

    Add files via upload
    Main python source file

---
+++ allyourlink.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+ #Ask user for input
+ site = input('Site address:')
+ #Search site
+url = 'https://'+site
+#Pull all links from website then parse output
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+#List all links pulled from site
+urls = []
+#Use recursion to enumerate all sites
+for link in soup.find_all('a'):
+    print(link.get('href'))
